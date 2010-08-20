1cd99d8efa9c39a68f4544a7790c0db7cd90e237
Leandro Francucci <francucci@itdelsat.com.ar>
2010-08-20 13:06:44 +0000

    Agrega soporte de locker2/3 y plunger2/3 en protocolo CTSAFE. Agrega dispositivos LOCKER2/3, PLUNGER2/3 y KEYSW a DEV_T y a pdevs[]. Modifica comando TLOCK y funcion proc_tlock(). Modifica comando TUNLOCKENABLE_LOCKS y funcion proc_tunlock_enable_locks(). Modifica comando TUNLOCKENABLE y funcion proc_tunlock_enable(). Modifica comando TLOCK y TUNLOCK y funcion validate_user_and_devlist() agregando search_dev_in_list().

--- CIM User Code/Sources/ctsafe/csdevs.h
+++ CIM User Code/Sources/ctsafe/csdevs.h
@@ -5,6 +5,10 @@
 
 #define LOCKER0_MASK		0x0002
 #define LOCKER1_MASK		0x0004
+#define LOCKER2_MASK		0x0008
+#define LOCKER3_MASK		0x0010
+
+#define NUM_DEVS_LIST		4
 
 
 /*
@@ -24,6 +28,11 @@ typedef enum
 	SAFEBOX,
 	STACKER0,
 	STACKER1,
+	LOCKER2,
+	LOCKER3,
+	PLUNGER2,
+	PLUNGER3,
+	KEYSW,
 
 	NUM_DEVICES
 } DEV_T;
