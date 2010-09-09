0979181509d66e0a839dde907e6d9c2448284e64
Dario Balina <balinad@itdelsat.com.ar>
2010-09-09 14:29:54 +0000

    Remapeados los pines Auxiliares en funcion del diseño PCB

--- CIM User Code/Sources/Drivers/Sensor/sensor.h
+++ CIM User Code/Sources/Drivers/Sensor/sensor.h
@@ -22,30 +22,30 @@
 /* 
  * Auxiliars Input Redefinition
  */
-#define LOCK3		AUXIO0
-#define D_LOCK3		D_AUXIO0
-#define P_LOCK3		P_AUXIO0
-#define LOCK3S		AUXIO1
-#define D_LOCK3S	D_AUXIO1
-#define P_LOCK3S	P_AUXIO1
-#define DOOR3S		AUXIO2
-#define D_DOOR3S	D_AUXIO2
-#define P_DOOR3S	P_AUXIO2
+#define LOCK3		AUXIO3
+#define D_LOCK3		D_AUXIO3
+#define P_LOCK3		P_AUXIO3
+#define LOCK3S		AUXIO4
+#define D_LOCK3S	D_AUXIO4
+#define P_LOCK3S	P_AUXIO4
+#define DOOR3S		AUXIO5
+#define D_DOOR3S	D_AUXIO5
+#define P_DOOR3S	P_AUXIO5
 
-#define LOCK4		AUXIO3
-#define D_LOCK4		D_AUXIO3
-#define P_LOCK4		P_AUXIO3
-#define LOCK4S		AUXIO4
-#define D_LOCK4S	D_AUXIO4
-#define P_LOCK4S	P_AUXIO4
-#define DOOR4S		AUXIO5
-#define D_DOOR4S	D_AUXIO5
-#define P_DOOR4S	P_AUXIO5
+#define LOCK4		AUXIO2
+#define D_LOCK4		D_AUXIO2
+#define P_LOCK4		P_AUXIO2
+#define LOCK4S		AUXIO1
+#define D_LOCK4S	D_AUXIO1
+#define P_LOCK4S	P_AUXIO1
+#define DOOR4S		AUXIO0
+#define D_DOOR4S	D_AUXIO0
+#define P_DOOR4S	P_AUXIO0
 
-#define LOCK3S_MASK	0x04
-#define DOOR3S_MASK	0x08
-#define LOCK4S_MASK	0x20
-#define DOOR4S_MASK	0x40
+#define LOCK3S_MASK	0x20
+#define DOOR3S_MASK	0x40
+#define LOCK4S_MASK	0x04
+#define DOOR4S_MASK	0x02
 
 #define AUXILIAR_IN	(PTBD&0x7E)
 
